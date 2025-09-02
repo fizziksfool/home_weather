ad2d2ef3d840e2ee69efbdafb2a6a17127fbbf12
fizziksfool <erbe.joshua@gmail.com>
2025-09-01 18:44:11 -0700

    Gets rid of the extra script to change .txt info

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-df = pd.read_csv(r"C:\Users\erbej\Documents\BME680 Temperature Logs\backyard data.csv")
+df = pd.read_csv(
+    r"C:\Users\erbej\Desktop\Python Projects\home_weather\home_weather.csv"
+)
 
 df["Time"] = pd.to_datetime(df["Time"].astype(str))
 
@@ -10,36 +12,3 @@ df.plot(x="Time", y="Pressure")
 df.plot(x="Time", y="Humidity")
 df.plot(x="Time", y="Gas")
 plt.show()
-
-
-# import csv
-# from datetime import datetime
-
-# input_file = r"C:\Users\erbej\Documents\BME680 Temperature Logs\2025-08-28 TEST.TXT"
-# output_file = (
-#     r"C:\Users\erbej\Documents\BME680 Temperature Logs\2025-08-28 TEST (fixed).TXT"
-# )
-
-# with (
-#     open(input_file, mode="r", newline="") as infile,
-#     open(output_file, mode="w", newline="") as outfile,
-# ):
-#     reader = csv.DictReader(infile)
-#     fieldnames = ["Time"] + [
-#         fn for fn in reader.fieldnames if fn not in ("Date", "Time")
-#     ]
-#     writer = csv.DictWriter(outfile, fieldnames=fieldnames)
-
-#     writer.writeheader()
-
-#     for row in reader:
-#         # Combine Date and Time into a single datetime string
-#         dt = datetime.strptime(row["Date"] + " " + row["Time"], "%m/%d/%Y %H:%M:%S")
-#         row_out = {"Time": dt.strftime("%Y-%m-%d %H:%M:%S")}
-
-#         # Add the rest of the columns
-#         for fn in fieldnames:
-#             if fn != "Time":
-#                 row_out[fn] = row[fn]
-
-#         writer.writerow(row_out)
